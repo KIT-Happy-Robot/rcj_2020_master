558cb6736f269adb62a5b5dedc44fb457dde983f
iidaissei <issei.288.higashi@icloud.com>
2020-02-24 15:51:44 +0900

    first_commit

---
+++ src/sm_ggi.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------
+# Title: RCJ2020_GoGetItの競技プログラム
+# Author: Issei Iida
+# Date: 2020/02/24
+#----------------------------------------------------------
+
+# Python
+import sys
+# ROS
+import rospy
+import smach
+import smach_ros
+from rcj_2020_ggi.srv import GgiTestPhase, GgiTestPhaseResponse
+
+sys.path.insert(0, '/home/athome/catkin_ws/src/mimi_common_pkg/scripts')
+from common_action_client import *
+from common_function import *
+
+
+class Training(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes = ['training_finish'])
+        # Service
+        self.training_srv() = rospy.ServiceProxy('/ggi_training_phase', Empty)
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: ENTER')
+        speak('Start GoGetIt')
+        # enterTheRoomAC(0.8)
+        self.training_srv()
+        speak('Start TestPhase')
+        return 'training_finish'
+
+
+class DecideMove(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes = ['decide_finish'])
+        # Subscriber
+        self.posi_sub = rospy.Subscriber('/navigation/move_place', String, self.currentPosiCB)
+        # Value
+        self.operator_coord = searchLocationName('operator')
+        self.current_position = 'operator'
+
+    def currentPosiCB(self, data):
+        self.current_position = data.data
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: DECIDE_MOVE')
+        if self.current_position != 'operator':
+            navigationAC(self.operator_coord)
+            speak('I arrived operator')
+        else:
+            pass
+        return 'decide_finish'
+
+
+class ListenCommand(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['listen_success',
+                                         'listen_failure',
+                                         'next_cmd',
+                                         'all_cmd_finish'],
+                             output_keys = ['cmd_out'])
+        # ServiceProxy
+        # self.listen_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        # Value
+        self.listen_count = 1
+        self.cmd_count = 1
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: LISTEN_COMMAND')
+        if self.cmd_count == 4:
+            speak('Finish all command')
+            return 'all_cmd_finish'
+        elif self.listen_count <= 3:
+            speak('CommandNumber is ' + str(self.cmd_count))
+            speak('ListenCount is ' + str(self.listen_count))
+            speak('Please instruct me')
+            # result = self.listen_srv()
+            result = True
+            if result:
+            # if result.result:
+                self.listen_count = 1
+                self.cmd_count += 1
+                userdata.cmd_out = result
+                return 'listen_success'
+            else:
+                self.listen_count += 1
+                speak("Sorry, I could't listen")
+                return 'listen_failure'
+        else:
+            speak("I couldn't understand the instruction")
+            self.listen_count = 1
+            self.cmd_count +=1
+            return 'next_cmd'
+
+
+class ExeAction(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['action_success',
+                                         'action_failure'],
+                             input_keys = ['cmd_in'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: EXE_ACTION')
+        # action = userdata.cmd_in.action
+        # data = userdata.cmd_in.data
+        action = ['go']
+        data = ['table']
+        result = exeActionPlanAC(action, data)
+        if result:
+            return 'action_success'
+        else:
+            return 'action_failure'
+
+
+def main():
+    sm_top = smach.StateMachine(outcomes = ['finish_sm_top'])
+    with sm_top:
+        smach.StateMachine.add(
+                'TRAINING',
+                Training(),
+                transitions = {'training_finish':'DECIDE_MOVE'})
+
+        smach.StateMachine.add(
+                'DECIDE_MOVE',
+                DecideMove(),
+                transitions = {'decide_finish':'LISTEN_COMMAND'})
+
+        smach.StateMachine.add(
+                'LISTEN_COMMAND',
+                ListenCommand(),
+                transitions = {'listen_success':'EXE_ACTION',
+                               'listen_failure':'LISTEN_COMMAND',
+                               'next_cmd':'DECIDE_MOVE',
+                               'all_cmd_finish':'finish_sm_top'},
+                remapping = {'cmd_out':'cmd'})
+
+        smach.StateMachine.add(
+                'EXE_ACTION',
+                ExeAction(),
+                transitions = {'action_success':'DECIDE_MOVE',
+                               'action_failure':'DECIDE_MOVE'},
+                remapping = {'cmd_in':'cmd'})
+
+    outcomes = sm_top.execute()
+
+
+if __name__ == '__main__':
+    rospy.init_node('sm_ggi', anonymous = True)
+    main()
---
+++ src/sm_gpsr.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------
+# Title: 2020JapanOpen_GPSR競技設計プログラム
+# Author: Issei Iida
+# Date: 2020/02/24
+#----------------------------------------------------------
+
+# Python
+import sys
+# ROS
+import rospy
+import smach
+import smach_ros
+from std_msgs.msg import String
+from std_srvs.srv import Trigger
+from ggi.srv import YesNo
+from gpsr.srv import ActionPlan
+from mimi_common_pkg.srv import ManipulateSrv
+
+sys.path.insert(0, '/home/athome/catkin_ws/src/mimi_common_pkg/scripts/')
+from common_function import speak, searchLocationName
+from common_action_client import (enterTheRoomAC,
+                                  navigationAC,
+                                  exeActionPlanAC)
+
+
+class Enter(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes = ['enter_finish'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: ENTER')
+        speak('Start GPSR')
+        # enterTheRoomAC(0.8)
+        return 'enter_finish'
+
+
+class DecideMove(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['decide_finish', 'all_cmd_finish'],
+                             input_keys = ['cmd_count_in'])
+        # Subscriber
+        self.posi_sub = rospy.Subscriber('/navigation/move_place', String, self.currentPosiCB)
+        # Value
+        self.operator_coord = searchLocationName('operator')
+        self.exit_coord = searchLocationName('entrance')
+        self.current_position = 'none'
+
+    def currentPosiCB(self, data):
+        self.current_position = data.data
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: DECIDE_MOVE')
+        if userdata.cmd_count_in == 4:
+            speak('Finish all command, Move to entrance')
+            navigationAC(self.exit_coord)
+            speak('Finish GPSR')
+            return 'all_cmd_finish'
+        elif self.current_position != 'operator':
+            navigationAC(self.operator_coord)
+            return 'decide_finish'
+        else:
+            return 'decide_finish'
+
+
+class ListenCommand(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['listen_success',
+                                         'listen_failure',
+                                         'next_cmd'],
+                             input_keys = ['cmd_count_in'],
+                             output_keys = ['cmd_out',
+                                            'cmd_count_out'])
+        # ServiceProxy
+        self.listen_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        self.yesno_srv = rospy.ServiceProxy('/yes_no', YesNo)
+        # Value
+        self.listen_count = 1
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: LISTEN_COMMAND')
+        cmd_count = userdata.cmd_count_in
+        if self.listen_count <= 3:
+            speak('CommandNumber is ' + str(cmd_count))
+            speak('ListenCount is ' + str(self.listen_count))
+            speak('Please instruct me')
+            result = self.listen_srv()
+            if result.result:
+                speak('Is this correct?')
+                answer = self.yesno_srv()
+                if answer.result:
+                    self.listen_count = 1
+                    cmd_count += 1
+                    userdata.cmd_out = result
+                    userdata.cmd_count_out = cmd_count
+                    return 'listen_success'
+                else:
+                    speak('Sorry')
+                    self.listen_count += 1
+                    return 'listen_failure'
+            else:
+                self.listen_count += 1
+                speak("I could't listen")
+                return 'listen_failure'
+        else:
+            speak("I couldn't understand the instruction")
+            self.listen_count = 1
+            cmd_count +=1
+            userdata.cmd_count_out = cmd_count
+            return 'next_cmd'
+
+
+class ExeAction(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['action_success',
+                                         'action_failure'],
+                             input_keys = ['cmd_in'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: EXE_ACTION')
+        action = userdata.cmd_in.action
+        data = userdata.cmd_in.data
+        print action
+        print data
+        result = exeActionPlanAC(action, data)
+        if result:
+            return 'action_success'
+        else:
+            return 'action_failure'
+
+
+def main():
+    sm_top = smach.StateMachine(outcomes = ['finish_sm_top'])
+    sm_top.userdata.cmd_count = 1
+    with sm_top:
+        smach.StateMachine.add(
+                'ENTER',
+                Enter(),
+                transitions = {'enter_finish':'DECIDE_MOVE'})
+
+        smach.StateMachine.add(
+                'DECIDE_MOVE',
+                DecideMove(),
+                transitions = {'decide_finish':'LISTEN_COMMAND',
+                               'all_cmd_finish':'finish_sm_top'},
+                remapping = {'cmd_count_in':'cmd_count'})
+
+        smach.StateMachine.add(
+                'LISTEN_COMMAND',
+                ListenCommand(),
+                transitions = {'listen_success':'EXE_ACTION',
+                               'listen_failure':'LISTEN_COMMAND',
+                               'next_cmd':'DECIDE_MOVE'},
+                remapping = {'cmd_out':'cmd',
+                             'cmd_count_in':'cmd_count',
+                             'cmd_count_out':'cmd_count'})
+
+        smach.StateMachine.add(
+                'EXE_ACTION',
+                ExeAction(),
+                transitions = {'action_success':'DECIDE_MOVE',
+                               'action_failure':'DECIDE_MOVE'},
+                remapping = {'cmd_in':'cmd'})
+
+    outcome = sm_top.execute()
+
+
+if __name__ == '__main__':
+    rospy.init_node('sm_gpsr', anonymous = True)
+    main()
